c61fddd367652371508f6c4ccb81f9c4ee112a91
kelele67 <kingand67@outlook.com>
2018-05-01 19:30:04 +0800

    training

--- README.md
+++ README.md
@@ -7,4 +7,4 @@
 
 ### Training a classifier
 
-### Data Parallelism
+
---
+++ Training/loss_and_optimizer.py
@@ -0,0 +1,31 @@
+import torch.optim as optim
+from network.convolution_neural_network import *
+from dataset import *
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
+
+# train the network
+
+for epoch in range(2): # loop over the dataset multiple times
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999: # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
